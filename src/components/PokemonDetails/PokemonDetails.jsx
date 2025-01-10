7b8209575ec6d0a5bcd0c12cc4feb31d95267f22
xavierchen <info@xavchen.com>
2025-01-10 16:55:09 +0000

    done

---
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -0,0 +1,27 @@
+// src/components/PokemonDetails/PokemonDetails.jsx
+import { useParams } from 'react-router';
+
+const PokemonDetails = (props) => {
+  const { pokemonId } = useParams();
+  console.log('pokemonId:', pokemonId);
+  // Always verify that any props are being passed correctly!
+  const singlePokemon = props.pokemon.find((poke) => (
+    poke._id === Number(pokemonId)
+  ));
+  console.log('Pokemon Object:', singlePokemon)
+  console.log(props); 
+
+  return (
+    <>
+      <h2>{singlePokemon.name}</h2>
+      <dl>
+        <dt>Weight:</dt>
+        <dd>{singlePokemon.weight}</dd>
+        <dt>Height:</dt>
+        <dd>{singlePokemon.height}</dd>
+      </dl>
+    </>
+  );
+};
+
+export default PokemonDetails;
